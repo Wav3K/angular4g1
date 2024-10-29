5b73c72249f390d5627dce541a37a1c4af70da30
Bartosz Wronowski <bartix320@gmail.com>
2024-10-29 12:10:46 +0100

    Validation added

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -16,8 +16,14 @@ export class TodoListComponent {
 //   adnotacja 1:
 //   defaultValue = "test";
   todos : Todo[] = [];
+  ErrorMessage : string = "";
 
   addTodo(todo : string) : void {
+    if(todo.length <=3){
+      this.ErrorMessage = "zadanie musi miec wiecej niz 3 znaki";
+      return;
+    }
+    this.ErrorMessage = "";
     this.todos.push({name: todo, isCompleted: false});
     console.log("Aktualna lista todo: ", this.todos);
   }
@@ -25,4 +31,11 @@ export class TodoListComponent {
   changeTodoStatus(todo: Todo) {
     todo.isCompleted = !todo.isCompleted;
   }
+
+  clearErrorMessage() {
+    this.ErrorMessage = "";
+  }
+
+  protected readonly name = name;
+
 }
