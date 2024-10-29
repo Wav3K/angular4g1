49ce45a6304a7e08f2d296aa98af685b1e7c76bd
Bartosz Wronowski <bartix320@gmail.com>
2024-10-29 13:01:30 +0100

    XD added

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -43,4 +43,10 @@ export class TodoListComponent {
   removeCompleted() {
      this.todos = this.todos.filter(item => !item.isCompleted);
   }
+
+  rotateThisShit() {
+    const body = document.querySelector("body") as HTMLBodyElement
+    body.style.transition = "2000ms ease";
+    body.style.transform = "rotate(360deg)";
+  }
 }
